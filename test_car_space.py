f2071ff13a165254e5c75ed91621f97c949ba59a
Brecht Govaerts <brecht.govaerts@student.ucll.be>
2025-12-16 13:50:19 +0100

    cars

--- test_car_space.py
+++ test_car_space.py
@@ -1,11 +1,9 @@
 import pygame
+import random
 
 pygame.init()
 WIDTH  = 1600
 HEIGHT = 800
-
-CAR_WIDTH = 200
-CAR_HEIGHT = 100
 
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -30,10 +28,22 @@ def draw_grid():
     for y in range(rows + 1):
         pygame.draw.line(screen, (70, 70, 70), (0, y * cell_size), (WIDTH, y * cell_size))
 
+def layout_cars_lvl_1():
+    CAR_WIDTH = 100
+    CAR_HEIGHT = 64
+
+    car_speed = 5
+
+    CAR_INTERVAL = 2000
+
+    return CAR_WIDTH, CAR_HEIGHT, car_speed, CAR_INTERVAL
+
 def draw_car(CARS):
     for car in CARS:
-        pygame.draw.rect(screen, "red", car)
-    
+        car = pygame.image.load("Images/orange_car.png")
+        car_rect = car.get_rect()
+        pygame.draw.rect(screen, car, car_rect)
+   
     pygame.display.update()
 
 
@@ -42,31 +52,35 @@ def main():
 
     clock = pygame.time.Clock()
 
+    cols2 = WIDTH // 64
+    rows2 = HEIGHT // 64
 
-
-    CAR_INTERVAL = 2000
     CAR_TIMER = 0
 
     CARS = []
 
+    if level == 1:
+        CAR_WIDTH, CAR_HEIGHT, car_speed, CAR_INTERVAL = layout_cars_lvl_1()
+     
     while running:
 
         screen.fill((30, 30, 30))
         draw_grid()
 
         CAR_TIMER += clock.tick(60)
-
-        if CAR_TIMER > CAR_INTERVAL :
-            car = pygame.Rect(-CAR_WIDTH, 400, CAR_WIDTH, CAR_HEIGHT)
+       
+        if CAR_TIMER > CAR_INTERVAL:
+            y = random.randint(0,HEIGHT) // CAR_HEIGHT
+            car = pygame.Rect(-CAR_WIDTH, y * CAR_HEIGHT, CAR_WIDTH, CAR_HEIGHT)
             CARS.append(car)
             CAR_TIMER = 0
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                running = False 
-        
+                running = False
+       
         for car in CARS[:]:
-            car.x += 2
+            car.x += car_speed
             if car.x > WIDTH:
                 CARS.remove(car)
 
